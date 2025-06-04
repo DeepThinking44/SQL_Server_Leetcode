276b9718893601cd9e8ecc54c865be3af3236266
PRADEEP SINGH <125987234+D6-80428-pradeep@users.noreply.github.com>
2025-06-04 16:07:03 +0530

    Time: 309 ms (63.54%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,17 +1,23 @@
-SELECT 
-    request_at AS Day,
+SELECT
+    CAST(T.request_at AS DATE) AS Day,
     ROUND(
-        SUM(CASE WHEN status <> 'completed' THEN 1.0 ELSE 0.0 END) / 
-        COUNT(*),
-        2
-    ) AS [Cancellation Rate]
-FROM 
-    Trips t
-WHERE 
-    request_at BETWEEN '2013-10-01' AND '2013-10-03'
-    AND client_id IN (SELECT users_id FROM Users WHERE banned = 'No')
-    AND driver_id IN (SELECT users_id FROM Users WHERE banned = 'No')
-GROUP BY 
-    request_at
-ORDER BY 
-    request_at;
+        CAST(SUM(CASE WHEN T.status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1 ELSE 0 END) AS DECIMAL(10, 2))
+        /
+        CAST(COUNT(T.id) AS DECIMAL(10, 2))
+    , 2) AS "Cancellation Rate"
+FROM
+    Trips AS T
+INNER JOIN
+    Users AS U_Client ON T.client_id = U_Client.users_id
+INNER JOIN
+    Users AS U_Driver ON T.driver_id = U_Driver.users_id
+WHERE
+    T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND U_Client.banned = 'No'
+    AND U_Driver.banned = 'No'
+GROUP BY
+    T.request_at
+HAVING
+    COUNT(T.id) >= 1
+ORDER BY
+    Day;
